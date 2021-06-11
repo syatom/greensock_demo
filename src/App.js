49d6e2221459a77644e4f9693d261e9783321775
Simon Piercy <53854902+syatom@users.noreply.github.com>
2021-06-11 15:34:05 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef} from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import fclogo from './img/fclogo.png' 
+import './App.css'
 
 function App() {
+
+//const header = useRef(null)
+const logo = useRef(null)
+
+gsap.registerPlugin(ScrollTrigger)
+
+  // useEffect(() => {
+  //   // Add animations to be played once page has loaded here
+  //   gsap.to(header.current, {color: '#05F775', duration: 2})
+  // }, [header])
+
+  useEffect(() => {
+    gsap.to(logo.current, {
+      rotate: 360,
+      scrollTrigger: {
+        trigger: logo.current,
+        markers: true,
+        start: 'top 60%',
+        end: 'bottom 40%',
+        scrub: 1,
+      },
+
+    })
+  }, [logo])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { /* <h1 ref={header}>Hello GSAP World!</h1> */}
+      <img className='fclogo' ref={logo} src={fclogo} />
     </div>
   );
 }
